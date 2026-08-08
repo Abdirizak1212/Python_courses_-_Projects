a307c0889d93548d1021ac95e67424af7ffd8030
Nadaara44 <aabdirisaaq042@gmail.com>
2026-08-08 11:31:19 +0300

    Week 2: string methods completed

---
+++ Courses/strings.py
@@ -0,0 +1,11 @@
+name = "    eng. nadaara    "
+print(name.strip())
+print(name.strip().title())
+print(name.upper())
+print(name.lower())
+print(name.replace(" nadaara"," Abdirizak"))
+
+sentence = "Python is easy and Python is powerful"
+print(sentence.count("Python"))
+print(sentence.startswith("Python"))
+print(sentence.replace("Python","AI"))
